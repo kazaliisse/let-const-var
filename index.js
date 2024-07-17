3938ad831d6451f715b836083f3e43c610676cdf
noor <noorhafowbare@gmail.com>
2024-07-17 14:57:59 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let age = 23;
+age = "23"
+
+const birthYear = 2002;
+// birthYear = 2002;
+console.log(birthYear)
+
+var job = 'programmer';
+job = 'programmer'
+
+lastName = "Noor";
+console.log(lastName)
